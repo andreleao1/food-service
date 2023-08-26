23bad3a15018430af3d6bd64703965af6b658433
andreleao1 <andre.gustavo.ls@hotmail.com>
2023-08-26 15:15:43 -0300

    feat:add-redis-configuration

--- src/main/java/br/com/agls/foodservice/infra/repository/FoodRepositoryImpl.java
+++ src/main/java/br/com/agls/foodservice/infra/repository/FoodRepositoryImpl.java
@@ -3,11 +3,14 @@ package br.com.agls.foodservice.infra.repository;
 import br.com.agls.foodservice.entity.Food;
 import br.com.agls.foodservice.infra.objectmapper.FoodObjectMapper;
 import br.com.agls.foodservice.infra.repository.interfaces.FoodRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 
+@Slf4j
 @Repository
 public class FoodRepositoryImpl implements FoodRepository {
 
@@ -22,8 +25,10 @@ public class FoodRepositoryImpl implements FoodRepository {
         this.jdbcTemplate.update("INSERT INTO food (id, name) VALUES (?,?)", food.getId().toString(), food.getName());
     }
 
+    @Cacheable(cacheNames = "foods", key = "#id")
     @Override
     public Food findById(String id) {
+        log.trace("Fetching data by database");
         return this.jdbcTemplate.queryForObject("SELECT id, name FROM food WHERE id = ?", new FoodObjectMapper(), id);
     }
 }
